fc89dbe8a7d328294e1f12ea9c2cb748c95ee2d6
atesfay <abrahamkt@yahoo.com>
2019-09-21 23:31:28 -0700

    Add component

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name: Abraham</div>
+                <div>Age: 25</div>
+                <div>Color: Blue</div>
+            </div>
+        );
+    }
+}
+
+export default Ninjas;
