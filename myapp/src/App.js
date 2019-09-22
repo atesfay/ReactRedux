fc8a85d031a9158d5ebb37cc6be85156764f0f3b
atesfay <abrahamkt@yahoo.com>
2019-09-21 23:06:06 -0700

    Add myapp

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My First React App</h1>
+     <p>Welcome :)</p>
+       
+    </div>
+  );
+}
+
+export default App;
