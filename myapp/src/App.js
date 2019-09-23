008bfd275d62fd2d9b80927c5ef1b3c8fcbdc686
atesfay <abrahamkt@yahoo.com>
2019-09-22 18:07:36 -0700

    Add lesson 15-17

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Abraham', age: 28, color: 'blue', id: 1 },
+      { name: 'Abraham', age: 20, color: 'blue', id: 1 },
       { name: 'Milkias', age: 26, color: 'gray', id: 2 },
       { name: 'David', age: 35, color: 'red', id: 3 }
     ]
@@ -16,6 +17,7 @@ class App extends Component {
         <h1>My First React App</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react';
 
-class Ninjas extends Component{
-    
-    render(){
-        // console.log(this.props)
-        // const {name, age, color} = this.props;
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja =>{
-            return(
+const Ninjas =({ninjas}) =>{
+
+    // console.log(this.props)
+    // const {name, age, color} = this.props;
+    // const {ninjas} = props;
+    const ninjaList = ninjas.map(ninja =>{
+        if (ninja.age > 20){
+            return (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Color: {ninja.color}</div>
                 </div>
             )
-        })
-        return(
-            <div className="ninja-list">
-            {ninjaList}
-            </div>
-        )
-    }
-}
+        } else {
+            return null
+        }
 
+    })
+    // we can also use terniray opreator 
+    // const ninjaList = ninjas.map( ninja => {
+    //     return ninja.age > 20 ? (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>Name: {ninja.name}</div>
+    //             <div>Age: {ninja.age}</div>
+    //             <div>Color: {ninja.color}</div>
+    //         </div>
+    //     ) :null;
+    // });
+    return(
+        <div className="ninja-list">
+        {ninjaList}
+        </div>
+    )
+}
 export default Ninjas;
